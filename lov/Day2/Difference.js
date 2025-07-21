a03e2bb04ff525a26f98bc53d6f8255b531b53ab
Lov-Bhardwaj <lov@cloud-rank.com>
2025-07-21 13:34:16 +0530

    Pushing Day1 and Day2

---
+++ lov/Day2/Difference.js
@@ -0,0 +1,11 @@
+// 1- Function Declaration- 
+//     When we declare a function using "function" keyword.
+//     Hoisting is allowed
+
+// 2- Function Expression 
+//     When the function is stored inside a variable
+//     Hositing is not allowed
+
+// 3- Arrow Function-
+//     Short hand way to declare a function using "=>"
+//     Hoisting is not allowed
